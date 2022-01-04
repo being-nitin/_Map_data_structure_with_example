309a456adcf4c6e8639354d3dbe85c9460b00ad3
Nitin <nsinghboss02@gmail.com>
2022-01-04 18:24:10 +0530

    Initial commit

---
+++ src/com/codechef/Main.java
@@ -0,0 +1,18 @@
+package com.codechef;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Map data structure:
+  HashMap <String,Integer> map = new HashMap<>();
+  map.put("nitin",20);
+        map.put("nitish",22);
+        map.put("alok",21);
+        map.put("alok",20);
+        map.put("ravit",23);
+        System.out.println(map);
+
+
+    }
+}
